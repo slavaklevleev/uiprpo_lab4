436cf030529221aaf5e3e113546816053d568bc5
slavaklevleev <slava.klevleev@gmail.com>
2023-03-31 18:23:39 +0300

    feat: age check

--- prog.py
+++ prog.py
@@ -10,3 +10,17 @@ for line in Lines:
         if idx == 0:
             name = re.sub(r'(?<=[a-z])([A-Z])', r' \1', elem).strip()
             newString.append(name)
+        if idx == 1:
+            age = re.search(r'[0-9]+', elem)
+            print(elem, name.string)
+            if age:
+                newString.append(age)
+            else:
+                newString.append('')
+
+        # if idx == 2:
+            # print('Телефон')
+        # if idx == 3:
+            # print('Email')
+
+    # print(arr)
