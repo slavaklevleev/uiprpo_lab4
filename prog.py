549b48c8ed485b2a956f84e4a9a51a2c4fc55a19
slavaklevleev <slava.klevleev@gmail.com>
2023-03-31 18:23:31 +0300

    feat: name correction

--- prog.py
+++ prog.py
@@ -1,6 +1,12 @@
+import re
+
 f = open("data.txt", "r")
 Lines = f.readlines()
 
 for line in Lines:
     arr = line.strip().split('|')
-    print(arr)
+    for idx, elem in enumerate(arr):
+        newString = []
+        if idx == 0:
+            name = re.sub(r'(?<=[a-z])([A-Z])', r' \1', elem).strip()
+            newString.append(name)
